f6403b65e0225db656fc08b78d0caa66ade01ab3
Rutuja shelar <122372944+rutujashelar2601@users.noreply.github.com>
2026-03-14 18:55:18 +0530

    Create DiamondPattern.java

---
+++ DiamondPattern.java
@@ -0,0 +1,31 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 5 - 1; i >= 1; i--) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            if (i > 1) {
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+ }
